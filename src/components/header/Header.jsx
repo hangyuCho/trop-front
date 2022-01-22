ee3144435cd1b47e570a35fdb7bc967a56b1cc7e
hangyuCho <chg3376@gmail.com>
2022-01-22 13:49:24 +0000

    add : init

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,9 @@
+import './header.css'
+
+const Header = () => {
+  return (
+    <div className="header"><span>hello world</span></div>
+  );
+};
+
+export default Header;
---
+++ src/components/main/Main.jsx
@@ -0,0 +1,9 @@
+import './main.css'
+
+const Main = () => {
+  return (
+    <div className="main"><span>hello world</span></div>
+  );
+};
+
+export default Main;
